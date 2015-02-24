3d1631e67b60394ec4c0fe9f5e2e4bacfeee4a86
Cody Hinchliff <cody.hinchliff@gmail.com>
2015-02-24 01:25:44 -0500

    add interface method

--- src/main/java/jade/tree/Tree.java
+++ src/main/java/jade/tree/Tree.java
@@ -3,8 +3,9 @@ package jade.tree;
 public interface Tree {
 
 	TreeNode getRoot();
-	Iterable<TreeNode> internalNodes(NodeOrder preorder);
+	Iterable<TreeNode> internalNodes(NodeOrder order);
 	Iterable<TreeNode> externalNodes();
+	Iterable<TreeNode> nodes(NodeOrder order);
 	int internalNodeCount();
 	int externalNodeCount();
 	TreeBipartition getBipartition(TreeNode p);
